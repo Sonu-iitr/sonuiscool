1af20fa8325712bfc0acde8bc780327506b91f09
Luvpreet042004 <121170040+Luvpreet042004@users.noreply.github.com>
2023-01-10 20:54:15 +0530

    First Commit

---
+++ marks.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+    int num;
+    cin>>num;
+    if(num>33){
+        if(num>90){
+            cout<<"A";
+        }
+        else if(num>80){
+            cout<<"B";
+        }
+        else if(num>70){
+                cout<<"C";
+        }
+        else if(num>60){
+                cout<<"D";
+        }
+        else if(num>33){
+                cout<<"E";
+        }
+    }
+    else {
+        cout<<"fail";
+    }
+    return 0;
+ }
